81cad2100a4069b778a4ae5efa7965633c2ea48e
blkjuptr <siyabongagundwana@gmail.com>
2018-10-12 00:14:25 +0200

    ui developers test

---
+++ js/validate.js
@@ -0,0 +1,32 @@
+//validate
+
+var form = {
+	validate: document.getElementById("validate"),
+	name: document.getElementById("name"),
+	email: document.getElementById("email"),
+	pass: document.getElementById("pass"),
+	date: document.getElementById("date")
+};
+
+//form submit
+form.validate.addEventListener("submit", CheckForm);
+
+//form submit validation
+var reEmail = /^[a-z0-9._%*]+@[a-z0-9.*]+\.[a-z]{2,4}$/;
+function CheckForm(e){
+	
+	var msg = "";
+	
+	//name
+	if((form.name.value == "") || (!reEmail.test(form.email.value) || form.email.value == "") || (form.pass.value < 8 || form.pass.value == "") || (form.date.value == "")){
+		swal('Something is missing');
+	}
+	
+	//message
+	else{
+		sweetAlert('Success! Good job!');
+	}
+	
+	
+	e.preventDefault();
+}
